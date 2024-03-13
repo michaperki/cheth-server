989b0a7ed641f362fcf370ef4ef6de81c06b0a9e
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-13 07:28:04 -0500

    adding .env to server
    .env contains PORT.
    also removed corsOptions

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 
-// Allow requests only from localhost:3000 during development
-const corsOptions = {
-  origin: 'http://localhost:3000',
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+}
+);
