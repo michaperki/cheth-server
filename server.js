0b16234c4aa7823fea61018d6b5da8fd0c0a2ff2
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-13 07:18:20 -0500

    update corsOptions

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(cors()); 
+
+// Allow requests only from localhost:3000 during development
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
