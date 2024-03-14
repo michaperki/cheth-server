917a2a753110e76e2a99846b3b4dd89d13788d8f
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-14 11:16:02 -0500

    ws debug

--- websocket.js
+++ websocket.js
@@ -8,6 +8,8 @@ function connectToWebSocketServer(server) {
     const wss = new WebSocket.Server({ server });
 
     wss.on('connection', (ws) => {
+        console.log('New WebSocket connection');
+
         ws.on('message', (message) => {
             console.log('Received message:', message);
             ws.send('Message received');
