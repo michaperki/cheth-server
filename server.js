87e6dedeb7835c84ff3626989cb1519f39be3046
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-14 11:10:40 -0500

    enable CORS for websocket

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app); // Create HTTP server
 
-// Connect to the WebSocket server
-require('./websocket').connectToWebSocketServer(server);
-
 app.use(express.json());
 app.use(cors());
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 5000;
+
+// Connect to the WebSocket server
+require('./websocket').connectToWebSocketServer(server);
+
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
@@ -31,3 +32,4 @@ db.connectToDatabase(
         console.error('Error connecting to the database:', error);
     }
 );
+
