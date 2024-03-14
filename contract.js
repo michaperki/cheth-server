3a89af4d2fc36f55c62ec3fd1fa6ec29075964fe
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-14 16:06:36 -0500

    tnkr

--- contract.js
+++ contract.js
@@ -5,7 +5,7 @@ const contractAddress = abi.networks[process.env.CHAIN_ID].address;
 const privateKey = process.env.SEPOLIA_PRIVATE_KEY;
 const wallet = new ethers.Wallet(privateKey);
 const signer = wallet.connect(new ethers.JsonRpcProvider(process.env.RPC_URL));
-const contract = new ethers.Contract(contractAddress, abi.abi, signer);
+const contract = new ethers.Contract(contractAddress, abi, signer);
 
 const startGame = async () => {
     try {
