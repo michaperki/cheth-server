b6379ae72ffe6a2d799284985bbcec99e1d53d53
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-15 10:05:19 -0500

    fix websocket import

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const db = require('./db');
 const routes = require('./routes');
 const http = require('http');
-const websocket = require('./websocket');
+const { websocket } = require('./websocket'); // Import the websocket function
 
 const app = express();
 const server = http.createServer(app);
