312eebd285be316739fb762bbb8f3b2f4ba3ceb1
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-19 03:47:38 -0500

    abi

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ const wss = require('./websocket'); // Assuming you export the WebSocket server 
 const ethers = require('ethers');
 
 
-const contractAddress = abi.networks[process.env.CHAIN_ID].address;
+const contractAddress = factoryContractAbi.networks[process.env.CHAIN_ID].address;
 const privateKey = process.env.SEPOLIA_PRIVATE_KEY;
 const wallet = new ethers.Wallet(privateKey);
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
