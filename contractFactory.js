ca233b8940cd4bf1b551bbda24c39ec98323af08
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-03-18 22:49:33 -0500

    fixed rpcProvider

--- contractFactory.js
+++ contractFactory.js
@@ -5,7 +5,7 @@ const db = require('./db'); // Import your database module
 const contractAddress = abi.networks[process.env.CHAIN_ID].address;
 const privateKey = process.env.SEPOLIA_PRIVATE_KEY;
 const wallet = new ethers.Wallet(privateKey);
-const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 const signer = wallet.connect(provider);
 const contract = new ethers.Contract(contractAddress, abi.abi, signer);
 
