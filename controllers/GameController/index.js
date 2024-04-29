0b1d920b2c0127be1bb9773b04c1ab36d6a4ca98
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-04-28 20:33:49 -0500

    change playGame to findOpponent

--- controllers/GameController/index.js
+++ controllers/GameController/index.js
@@ -1,6 +1,6 @@
 // gameController.js
 
-const playGame = require('./playGame');
+const findOpponent = require('./findOpponent');
 const startGame = require('./startGame');
 const { getGame, getGames, getAllGames } = require('./fetchGame');
 const { cancelGamePairing, cancelGame, reportGameOver, forceDraw, deleteGame } = require('./endGame');
@@ -8,21 +8,21 @@ const { refreshContractBalance, getGameCount, getTotalWagered } = require('./uti
 const { requestRematch, acceptRematch, declineRematch, cancelRematch } = require('./rematch');
 
 module.exports = {
-    playGame,
-    startGame,
-    getGame,
-    getGames,
-    getAllGames,
-    cancelGamePairing,
-    cancelGame,
-    reportGameOver,
-    forceDraw,
-    deleteGame,
-    refreshContractBalance,
-    getGameCount,
-    getTotalWagered,
-    requestRematch,
-    acceptRematch,
-    declineRematch,
-    cancelRematch
+	findOpponent,
+	startGame,
+	getGame,
+	getGames,
+	getAllGames,
+	cancelGamePairing,
+	cancelGame,
+	reportGameOver,
+	forceDraw,
+	deleteGame,
+	refreshContractBalance,
+	getGameCount,
+	getTotalWagered,
+	requestRematch,
+	acceptRematch,
+	declineRematch,
+	cancelRematch
 };
