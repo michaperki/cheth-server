1d3b6c71c1446fbdb78724196f9fae5cbdb20c04
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-05-14 18:01:09 -0500

    Revert "split server into server and app"
    This reverts commit 82abe29c4c1a5614816b662f6cec2024f69c2ae3.

--- server.js
+++ server.js
@@ -1,15 +1,68 @@
-const http = require('http');
-const app = require('./app');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const db = require('./db');
+const http = require('http');
 const websocket = require('./websocket'); // Import the websocket function
-const { logger } = require('./utils/LoggerUtils');
+const { logger, expressLogger } = require('./utils/LoggerUtils'); // Import the logger instance and expressLogger middleware
+const router = require('./routes');
+const path = require('path');
+const fs = require('fs');
 
+const app = express();
 const server = http.createServer(app);
 
-// Initialize WebSocket and attach it to the app
+
+app.use(express.json());
+app.use(expressLogger); // Use Pino middleware for logging
+
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+// Initialize WebSocket and get the instance
 const { wss, clients } = websocket(server);
-app.wss = wss;
-app.clients = clients;
+
+// Middleware to inject WebSocket instance into request object
+app.use((req, res, next) => {
+    req.wss = wss;
+    req.clients = clients;
+    next();
+});
+
+// Custom middleware for logging only necessary requests
+app.use((req, res, next) => {
+    if (req.originalUrl !== '/crypto/ethToUsd') {
+        logger.info('Request completed', {
+            req,
+            res
+        });
+    }
+    next();
+});
+
+// add a route to get the icons
+// the icons route should serve all the icons in the icons folder
+app.use('/icons', express.static(path.join(__dirname, 'icons')));
+
+app.use('/allIcons', (req, res) => {
+    const iconsDir = path.join(__dirname, 'icons');
+
+    fs.readdir(iconsDir, (err, files) => {
+        if (err) {
+            console.error('Error reading icons directory:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const iconFiles = files.filter(file => /\.(svg)$/i.test(file));
+        res.json({ icons: iconFiles });
+    });
+});
+
+app.use(router);
 
 const PORT = process.env.PORT || 5000;
 
@@ -26,5 +79,4 @@ db.connectToDatabase(
     }
 );
 
-module.exports = server;
-
+module.exports = app;
