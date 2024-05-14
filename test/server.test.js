df4bfa5b6730815d47dee04c679ba96de77ffc36
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-05-14 18:02:25 -0500

    Revert "server and app initial tests pass"
    This reverts commit 37319fe903be1c3cdc7d2269de3f2ea6edbbdaa4.

--- test/server.test.js
+++ test/server.test.js
@@ -1,49 +1,26 @@
 const request = require('supertest');
-const http = require('http');
-const { app } = require('../server'); // Adjust the path to your server file
-const sinon = require('sinon');
-const db = require('../db'); // The actual db module
-const mockDb = require('./mocks/db'); // The mock db module
+const app = require('../server'); // Adjust the path to your server file
 
 let expect;
-let server;
 
 describe('Server', function () {
     before(async function () {
         // Dynamically import chai
         const chai = await import('chai');
         expect = chai.expect;
-
-        // Replace the actual db module with the mock
-        sinon.replace(db, 'connectToDatabase', mockDb.connectToDatabase);
-
-        // Start the server on a random available port
-        server = http.createServer(app);
-        await new Promise(resolve => server.listen(0, resolve));
     });
 
-    after(function (done) {
-        // Restore the original db module
-        sinon.restore();
-
-        // Close the server after tests
-        if (server && server.listening) {
-            server.close(done);
-        } else {
-            done();
-        }
-    });
-
-    it('should connect to the database', function (done) {
-        db.connectToDatabase(
-            () => {
-                expect(true).to.be.true;
+    it('should return a list of icons on GET /allIcons', function (done) {
+        request(app)
+            .get('/allIcons')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) return done(err);
+                expect(res.body).to.be.an('object');
+                expect(res.body.icons).to.be.an('array');
                 done();
-            },
-            (error) => {
-                done(error);
-            }
-        );
+            });
     });
 });
 
