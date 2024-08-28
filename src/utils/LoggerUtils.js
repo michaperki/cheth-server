3367d9c9b4ecc9664d924fc0005e295f7aa8d150
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-08-28 10:38:27 -0500

    revert logger utils to js

---
+++ src/utils/LoggerUtils.js
@@ -0,0 +1,26 @@
+const pino = require('pino');
+const expressPino = require('pino-http');
+
+// Create a Pino logger instance
+const logger = pino({
+    level: 'info', // Set log level to 'info' (default is 'info')
+    transport: {
+        target: 'pino-pretty'
+    },
+    
+    redact: {
+        paths: [
+            'req.headers', 
+            'req.connection.remoteAddress',
+            'req.connection.remotePort',
+            'req.connection.remoteAddress',
+            'res.headers',
+            ], // Redact sensitive information
+        remove: true, // Remove the redacted fields completely from the log
+    },
+});
+
+// Create an Express middleware with Pino logger
+const expressLogger = expressPino({ logger });
+
+module.exports = { logger, expressLogger };
