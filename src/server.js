dce87ea0d6c6e61632e594b77f0d70a169f2eb8c
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-09-01 15:28:33 -0500

    clean up server logs

--- src/server.js
+++ src/server.js
@@ -1,84 +1,63 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const db = require("./db");
 const http = require("http");
-const websocket = require("./websocket"); // Import the websocket function
-const { logger, expressLogger } = require("./../dist/utils/LoggerUtils"); // Import the logger instance and expressLogger middleware
-const router = require("./routes");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
+const { logger, expressLogger } = require("./../dist/utils/LoggerUtils");
+const db = require("./db");
+const websocket = require("./websocket");
+const router = require("./routes");
 
 const app = express();
 const server = http.createServer(app);
 
+// Middleware
 app.use(express.json());
-app.use(expressLogger); // Use Pino middleware for logging
+app.use(expressLogger);
 
-console.log("CORS_ORIGIN");
-console.log(process.env.CORS_ORIGIN);
-
+// CORS configuration
 const corsOptions = {
   origin: process.env.CORS_ORIGIN,
   optionsSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 
-// Initialize WebSocket and get the instance
+// WebSocket setup
 const { wss, clients } = websocket(server);
-
-// Middleware to inject WebSocket instance into request object
 app.use((req, res, next) => {
   req.wss = wss;
   req.clients = clients;
   next();
 });
 
-// Custom middleware for logging only necessary requests
-app.use((req, res, next) => {
-  if (req.originalUrl !== "/crypto/ethToUsd") {
-    logger.info("Request completed", {
-      req,
-      res,
-    });
-  }
-  next();
-});
-
-// add a route to get the icons
-// the icons route should serve all the icons in the icons folder
+// Static file serving
 app.use("/icons", express.static(path.join(__dirname, "icons")));
 
-app.use("/allIcons", (req, res) => {
-  const iconsDir = path.join(__dirname, "icons");
-
-  fs.readdir(iconsDir, (err, files) => {
-    if (err) {
-      console.error("Error reading icons directory:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-
-    const iconFiles = files.filter((file) => /\.(svg)$/i.test(file));
+// Routes
+app.use("/allIcons", async (req, res) => {
+  try {
+    const iconsDir = path.join(__dirname, "icons");
+    const files = await fs.readdir(iconsDir);
+    const iconFiles = files.filter((file) => /\.svg$/i.test(file));
     res.json({ icons: iconFiles });
-  });
+  } catch (err) {
+    logger.error("Error reading icons directory:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.use(router);
 
+// Server initialization
 const PORT = process.env.PORT || 5000;
-
 server.listen(PORT, () => {
   logger.info(`Server listening on port ${PORT}`);
 });
 
-db.connectToDatabase(
-  () => {
-    logger.info("Connected to the database");
-  },
-  (error) => {
-    logger.error("Error connecting to the database:", error);
-  },
-);
+// Database connection
+db.connectToDatabase()
+  .then(() => logger.info("Connected to the database"))
+  .catch((error) => logger.error("Error connecting to the database:", error));
 
 module.exports = app;
