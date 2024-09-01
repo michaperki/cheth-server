1c72d93a886d14bb526aae21af9bb53b16d700fd
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-09-01 15:24:43 -0500

    update logger

--- src/utils/LoggerUtils.js
+++ src/utils/LoggerUtils.js
@@ -6,22 +6,59 @@ const LOG_LEVEL = process.env.LOG_LEVEL || 'error';
 const logger = pino({
     level: LOG_LEVEL,
     transport: {
-        target: 'pino-pretty'
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+        }
     },
-    
     redact: {
         paths: [
-            'req.headers', 
-            'req.connection.remoteAddress',
-            'req.connection.remotePort',
-            'req.connection.remoteAddress',
-            'res.headers',
-            ], // Redact sensitive information
-        remove: true, // Remove the redacted fields completely from the log
+            'req.headers',
+            'req.remoteAddress',
+            'req.remotePort',
+            'res.headers'
+        ],
+        remove: true,
     },
 });
 
+// Create a custom serializer for requests
+const customReqSerializer = (req) => ({
+    method: req.method,
+    url: req.url,
+});
+
+// Create a custom serializer for responses
+const customResSerializer = (res) => ({
+    statusCode: res.statusCode
+});
+
 // Create an Express middleware with Pino logger
-const expressLogger = expressPino({ logger });
+const expressLogger = expressPino({
+    logger,
+    serializers: {
+        req: customReqSerializer,
+        res: customResSerializer
+    },
+    // Only log if the response status code is 400 or higher
+    customLogLevel: function (res, err) {
+        if (res.statusCode >= 400 || err) {
+            return 'error'
+        }
+        return 'silent'
+    },
+    // Customize the objects included in the log
+    customSuccessMessage: function (res) {
+        if (res.statusCode >= 400) {
+            return `request errored with status code: ${res.statusCode}`
+        }
+        return `request completed`
+    },
+    customErrorMessage: function (error, res) {
+        return `request errored with status code: ${res.statusCode}`
+    },
+});
 
 module.exports = { logger, expressLogger };
